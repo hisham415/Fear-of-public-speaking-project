a81227109c8358bb5b4d1adc0cfa47e48bcd88fa
hisham415 <hishamsayed444@gamail.com>
2022-05-27 02:48:45 +0200

    gg

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
 
+//mongodb://127.0.0.1:27017/task-api
 
-mongoose.connect('mongodb://127.0.0.1:27017/task-api',{
+mongoose.connect('mongodb+srv://root:toor@cluster0.4v6ca.mongodb.net/test',{
     useNewUrlParser: true,
     //useCreateIndex: true
 })
